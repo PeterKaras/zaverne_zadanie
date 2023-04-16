866384689766e3e8d4a07a16ad941868b364e95c
PeterKaras <peter.karas.mail@gmail.com>
2023-04-16 23:06:46 +0200

    authMe updated, returning everything, UserController updated

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -41,4 +41,110 @@ class UsersController extends AbstractController
 
         return new JsonResponse($data);
     }
+
+    #[Route('/', methods: 'GET')]
+    #[IsGranted("ROLE_ADMIN")]
+    public function index(): JsonResponse
+    {
+        $users = $this->userRepository->findAll();
+        $data = [];
+
+        foreach ($users as $user) {
+            $data[] = [
+                'id' => $user->getId(),
+                'email' => $user->getEmail(),
+                'roles' => $user->getRoles(),
+            ];
+        }
+
+        return new JsonResponse($data);
+    }
+
+    #[Route('/{id}', methods: 'GET')]
+    #[IsGranted("ROLE_ADMIN")]
+    public function show(int $id): JsonResponse
+    {
+        $user = $this->userRepository->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        $data = [
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            'roles' => $user->getRoles(),
+        ];
+
+        return new JsonResponse($data);
+    }
+
+    #[Route('/', methods: 'POST')]
+    #[IsGranted("ROLE_ADMIN")]
+    public function create(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $user = new User();
+        $user->setEmail($data['email']);
+        $user->setPassword($data['password']);
+        $user->setRoles($data['roles']);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        $response = new JsonResponse([
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            'roles' => $user->getRoles(),
+        ]);
+        $response->setStatusCode(Response::HTTP_CREATED);
+
+        return $response;
+    }
+
+    #[Route('/{id}', methods: 'PUT')]
+    #[IsGranted("ROLE_ADMIN")]
+    public function update(Request $request, int $id): JsonResponse
+    {
+        $user = $this->userRepository->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        $data = json_decode($request->getContent(), true);
+        $user->setEmail($data['email']);
+        $user->setRoles($data['roles']);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->flush();
+
+        $response = new JsonResponse([
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            'roles' => $user->getRoles(),
+        ]);
+        $response->setStatusCode(Response::HTTP_OK);
+
+        return $response;
+    }
+
+    #[Route('/{id}', methods: 'DELETE')]
+    #[IsGranted("ROLE_ADMIN")]
+    public function delete(int $id): JsonResponse
+    {
+        $user = $this->userRepository->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
 }
